57919c4ebc644c616b9fea0d2ac32d62ff9b2d56
Chirag Mathur <54184768+chiragmathur03@users.noreply.github.com>
2023-06-19 19:47:41 +0530

    Fix Errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,72 +1,103 @@
-import React, { useEffect }from 'react';
+import React, { useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import avatar from '../data/avatar.jpg';
+// import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-  <TooltipComponent content={title} position='BottomCenter'>
-    <button type='button' onClick={customFunc} style={{ color }} className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
-
-    <span style={{ background: dotColor }} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'/>
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={() => customFunc()}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
       {icon}
     </button>
   </TooltipComponent>
-)
+);
 
-const NavBar = () => { 
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked ,handleClick, screenSize, setScreenSize, currentColor } = useStateContext();
+const Navbar = () => {
+  const { currentColor, activeMenu, setActiveMenu, handleClick, setScreenSize, screenSize } = useStateContext();
+  // const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
-  useEffect (() => {
-    const handleResize = () => setScreenSize (window.innerWidth);
-    window.addEventListener('resize',handleResize);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
 
     handleResize();
 
-    return () => window.removeEventListener ('resize', handleResize);
-  }, []);
+    return () => window.removeEventListener('resize', handleResize);
+  });
 
-  useEffect (() => {
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+  
+    window.addEventListener('resize', handleResize);
+  
+    handleResize();
+  
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setScreenSize]);
+  
+  useEffect(() => {
     if (screenSize <= 900) {
       setActiveMenu(false);
-    }else{
+    } else {
       setActiveMenu(true);
     }
-  }, [screenSize]);
+  }, [screenSize, setActiveMenu]);
+
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton title='Menu' customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color={currentColor} icon={<AiOutlineMenu />}/>
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
-    <div className='flex'>
-      <NavButton title='Cart' customFunc={() => handleClick ('cart')} color={currentColor} icon={<FiShoppingCart />}/>
-      <NavButton title='Chat' dotColor="#03C9D7" customFunc={() => handleClick('chat')} color={currentColor} icon={<BsChatLeft />}/>
-      <NavButton title='Notifications' dotColor="#03C9D7" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />}/>
-      <TooltipComponent content='Profile' position='BottomCenter'>
-        <div className='flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={() => handleClick('userProfile')}>
-          <div className='flex items-center'>
-          <img className='rounded-full w-8 h-8' src={avatar}/>
-          <p className='ml-3'>
-            <span className='text-gray-400 text-14'>Hi,</span>{' '}
-            <span className='text-gray-400 font-body ml-1 text-14'> Michael </span>
-          </p>
-          <MdKeyboardArrowDown className='text-gray-400 text-14' />
+      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
+      <div className="flex">
+        <NavButton title="Cart" customFunc={() => handleClick('cart')} color={currentColor} icon={<FiShoppingCart />} />
+        <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color={currentColor} icon={<BsChatLeft />} />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
-        </div>
-      </TooltipComponent>
-
-      {/* {isClicked.cart && <Cart />}
-      {isClicked.chat && <Chat />}
-      {isClicked.notification && <Notification />}
-      {isClicked.userprofile && <UserProfile />} */}
+        </TooltipComponent>
+{/* 
+        {isClicked.cart && (<Cart />)}
+        {isClicked.chat && (<Chat />)}
+        {isClicked.notification && (<Notification />)}
+        {isClicked.userProfile && (<UserProfile />)} */}
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default NavBar
+export default Navbar;
